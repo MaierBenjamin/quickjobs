944e5f4c6ae44c72c34dce098d1d0629fbecd314
arberidri <arber_idrizaj@sluz.ch>
2026-02-05 17:26:11 +0100

    homepage angefangen

--- quickjobs/src/app/scr/pages/home/home.ts
+++ quickjobs/src/app/scr/pages/home/home.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css'],
 })
 export class Home {
+  items = [
+    { title: 'Haushaltshilfe', img: 'https://images.unsplash.com/photo-1581578731548-c64695cc6952' },
+    { title: 'Gartenarbeit', img: 'https://images.unsplash.com/photo-1599687351724-dfa3c4ff81b1' },
+    { title: 'Umzugshilfe', img: 'https://images.unsplash.com/photo-1600585154340-be6161a56a0c' },
+    { title: 'Hundesitting', img: 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b' },
+  ];
 
+  index = 0;
+
+  get visibleItems() {
+    return this.items.slice(this.index, this.index + 3);
+  }
+
+  next() {
+    if (this.index < this.items.length - 3) this.index++;
+  }
+
+  prev() {
+    if (this.index > 0) this.index--;
+  }
 }
