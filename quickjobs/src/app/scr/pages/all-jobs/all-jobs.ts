9fae80a1d1ce1d1693447e90fcf5f2e2842b355f
arberidri <arber_idrizaj@sluz.ch>
2026-02-05 15:38:26 +0100

    alle pages erstellt

---
+++ quickjobs/src/app/scr/pages/all-jobs/all-jobs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-jobs',
+  imports: [],
+  templateUrl: './all-jobs.html',
+  styleUrl: './all-jobs.css',
+})
+export class AllJobs {
+
+}
